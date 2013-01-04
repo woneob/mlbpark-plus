3b8769d47730f34b93e340da7833192390bcb2eb
woneob <1up@1upz.com>
2013-01-04 19:25:02 +0900

    #23 툴바 디자인 변경에 필요한 클래서 추가, restore 시 불필요한 항목 제거
    checkbox는 기본적으로 체크가 되어있지 않으므로 체크가 되어야 할 항목들만 확인하면 되지 않을까요.
    checkbox의 부모 엘리먼트에 .checked 를 추가한 것은 CSS에서 선택하기 애매한 부분이 있어 디자인 변경상 필요한 것
    입니다. .change() 이벤트가 있어 그 부분에 살짝 끼워 둡니다.

--- javascripts/popup.js
+++ javascripts/popup.js
@@ -2,38 +2,30 @@
 var extensionInfo;
 
 /**
- * 저장된 내용을 가져와서 화면에 보여준다.
+ * 저장된 내용을 가져와서 화면에 보여준다. 활성화 항목만 체크 표시.
  */
 function restore() {
 	if (localStorage['titIcon'] == 1 || localStorage['titIcon'] == null) {
 		document.getElementById('titIcon').checked = true;
-	} else {
-		document.getElementById('titIcon').checked = false;
 	}
 
 	if (localStorage['team'] == 1 || localStorage['team'] == null) {
 		document.getElementById('team').checked = true;
-	} else {
-		document.getElementById('team').checked = false;
 	}
 
-	if (localStorage['block'] == 0 || localStorage['block'] == null) {
-		document.getElementById('block').checked = false;
-	} else {
+	if (localStorage['block'] == 1) {
 		document.getElementById('block').checked = true;
 	}
 
-	if (localStorage['blockUser'] == 0 || localStorage['blockUser'] == null) {
-		document.getElementById('blockUser').checked = false;
-	} else {
+	if (localStorage['blockUser'] == 1) {
 		document.getElementById('blockUser').checked = true;
 	}
 
 	if (localStorage['userHistory'] == 1) {
 		document.getElementById('userHistory').checked = true;
-	} else {
-		document.getElementById('userHistory').checked = false;
 	}
+
+	$(':checked').parent().addClass('checked');
 }
 
 /**
@@ -65,15 +57,19 @@ function bindEvent() {
 	// 옵션 체크박스 - 모든 체크박스에 이벤트 바인딩
 	$(':checkbox').bind('change', function(event) {
 		var $messageBox = $('#status #message');
-		if(this.checked) localStorage[this.id] = 1;
-		else localStorage[this.id] = 0;
+		if (this.checked) {
+			localStorage[this.id] = 1;
+			$(this).parent().addClass('checked');
+		} else {
+			localStorage[this.id] = 0;
+			$(this).parent().removeClass('checked');
+		}
 		$messageBox.text('저장되었습니다.').stop(true, true).show();
 		setTimeout(function() {
 			$messageBox.fadeOut('slow');
 		}, 1000);
 	});
 }
-
 
 $(window).load(function(){
 	// extension 정보를 가져와서 버전을 보여준다.
