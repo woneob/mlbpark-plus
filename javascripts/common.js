0ecd22a628695c0784a6547897bf9a7191505ab8
Cho Won-eob <1up@1upz.com>
2013-05-04 22:47:01 +0900

    반응형 레이아웃 원복

--- javascripts/common.js
+++ javascripts/common.js
@@ -18,47 +18,4 @@ $(document).ready(function() {
 	$(document.body).on('click',function(){
 		$('div[id^="nik_"]').css('display','none');
 	});
-
-	var wrap = document.getElementById('wrap');
-	var navi = document.getElementById('navi');
-	var naveList = $(navi).html();
-	navi.getElementsByClassName('navi_bar')[0].insertAdjacentHTML('beforeEnd','<li id="moreMenu"><button id="moreMenuBtn" type="button">open</button></li>');
-	var btn_moreMenu = document.getElementById('moreMenuBtn');
-	status = btn_moreMenu.className;
-
-	function AdjustMenu() {
-		var windowWidth = document.body.clientWidth;
-		if (windowWidth < 580) {
-			$(btn_moreMenu).on('click', function(){
-				if (status == '' || status == 'closed') {
-					CreateMoreMenu();
-				} else {
-					RemoveMoreMenu();
-				}
-			});
-		} else {
-			RemoveMoreMenu();
-		}
-	}
-
-	function CreateMoreMenu() {
-		wrap.insertAdjacentHTML('beforeEnd','<div id="popMenu">' + naveList + '</div>');
-		btn_moreMenu.className = 'opened';
-		status = 'opened';
-	}
-
-	function RemoveMoreMenu() {
-		$('#popMenu').remove();
-		btn_moreMenu.className = 'closed';
-		status = 'closed';
-	}
-
-	AdjustMenu();
-	$(window).resize(AdjustMenu);
 });
-
-
-
-
-
-
