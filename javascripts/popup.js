647b9f990a26811c9f2a8ca3e0a4802cd4ec812e
Cho Won-eob <1up@1upz.com>
2013-05-07 02:56:35 +0900

    popup 자바스크립트 일부를 native 로 변경

--- javascripts/popup.js
+++ javascripts/popup.js
@@ -5,27 +5,16 @@ var extensionInfo;
  * 저장된 내용을 가져와서 화면에 보여준다. 활성화 항목만 체크 표시.
  */
 function restore() {
-	if (localStorage['titIcon'] == 1 || localStorage['titIcon'] == null) {
-		document.getElementById('titIcon').checked = true;
-	}
+	if (localStorage['titIcon'] == 1 || localStorage['titIcon'] == null) document.getElementById('titIcon').checked = true;
+	if (localStorage['team'] == 1 || localStorage['team'] == null) document.getElementById('team').checked = true;
+	if (localStorage['block'] == 1) document.getElementById('block').checked = true;
+	if (localStorage['blockUser'] == 1) document.getElementById('blockUser').checked = true;
+	if (localStorage['userHistory'] == 1) document.getElementById('userHistory').checked = true;
 
-	if (localStorage['team'] == 1 || localStorage['team'] == null) {
-		document.getElementById('team').checked = true;
+	var checkedEl = document.querySelectorAll(':checked');
+	for (var i=0; i < checkedEl.length; i++) {
+		checkedEl[i].parentNode.classList.add('checked');
 	}
-
-	if (localStorage['block'] == 1) {
-		document.getElementById('block').checked = true;
-	}
-
-	if (localStorage['blockUser'] == 1) {
-		document.getElementById('blockUser').checked = true;
-	}
-
-	if (localStorage['userHistory'] == 1) {
-		document.getElementById('userHistory').checked = true;
-	}
-
-	$(':checked').parent().addClass('checked');
 }
 
 /**
@@ -33,19 +22,26 @@ function restore() {
  */
 function bindEvent() {
 	// 제목 차단
-	$('#blockBtn').on('click', function() {
-		var blockVar = $('#blockInput').val();
-		if('' != blockVar) {
+	var titleBlockInput = document.getElementById('blockInput');
+	var titleBlockBtn = document.getElementById('blockBtn');
+
+	titleBlockBtn.onclick = function(){
+		var blockVar = titleBlockInput.value;
+		if ('' != blockVar) {
 			window.postMessage({
-				action:'titleBlockDelivery',
+				action: 'titleBlockDelivery',
 				title: blockVar,
 				input: 'blockInput'
 			}, '*');
 		}
-	});
+	}
+
 	// 사용자 차단
-	$('#blockUserBtn').on('click', function() {
-		var blockUserVar = $('#blockUserInput').val();
+	var userBlockInput = document.getElementById('blockUserInput');
+	var userBlockBtn = document.getElementById('blockUserBtn');
+
+	userBlockBtn.onclick = function(){
+		var blockUserVar = userBlockInput.value;
 		if('' != blockUserVar) {
 			window.postMessage({
 				action:'userBlockDelivery',
@@ -53,7 +49,7 @@ function bindEvent() {
 				input: 'blockUserInput'
 			}, '*');
 		}
-	});
+	}
 
 	$('.txtInput').keyup(function(event){
 		if(event.keyCode == 13){
@@ -63,13 +59,13 @@ function bindEvent() {
 
 	// 옵션 체크박스 - 모든 체크박스에 이벤트 바인딩
 	$(':checkbox').on('change', function(event) {
-		var $messageBox = $('#status #message');
+		var $messageBox = $('#message');
 		if (this.checked) {
 			localStorage[this.id] = 1;
-			$(this).parent().addClass('checked');
+			this.parentNode.classList.add('checked');
 		} else {
 			localStorage[this.id] = 0;
-			$(this).parent().removeClass('checked');
+			this.parentNode.classList.remove('checked');
 		}
 		$messageBox.text('저장되었습니다.').stop(true, true).show();
 		setTimeout(function() {
@@ -78,11 +74,11 @@ function bindEvent() {
 	});
 }
 
-$(window).load(function(){
+$(document).ready(function(){
 	// extension 정보를 가져와서 버전을 보여준다.
 	chrome.management.get(chrome.i18n.getMessage('@@extension_id'), function(result) {
 		extensionInfo = result;
-		$('#version').text('ver. ' + extensionInfo.version);
+		document.getElementById('version').textContent = 'ver. ' + extensionInfo.version;
 	});
 
 	restore();
