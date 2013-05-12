48b952f19dd3f098c432cc2ad64df314171abd2e
Cho Won-eob <1up@1upz.com>
2013-05-12 15:12:20 +0900

    AdWrapper, AdFrame 제거 방식 변경

--- javascripts/common.js
+++ javascripts/common.js
@@ -1,21 +1,32 @@
+var doc = document;
 chrome.extension.sendMessage({action:'width'}, function(response) {
 	var widthVar = response.width,
 	widthValVar = response.widthVal;
 
 	//custom container width
 	if (widthVar == '1') {
-		var customWidth = document.createElement('style'); 
-		customWidth.appendChild(document.createTextNode('#wrap {max-width:' + widthValVar + 'px !important;}'));
-		document.documentElement.insertBefore(customWidth);
+		var customWidth = doc.createElement('style'); 
+		customWidth.appendChild(doc.createTextNode('#wrap {max-width:' + widthValVar + 'px !important;}'));
+		doc.documentElement.insertBefore(customWidth);
 	}
 });
 
-$(document).ready(function() {
-	//remove items
-	$('.ad_left_w,.ad_left_w2,.ad_right_w,script[src^="http://cad.donga.com/"],script[src^="http://mlbpark.donga.com/acecounter/"],iframe[src^="http://ar.donga.com/"],iframe[src^="http://idolpark.donga.com/"],iframe[src^="http://sports.donga.com/"],iframe[src="http://mlbpark.donga.com/mypage/memo_read.php"],iframe[src="http://mlbpark.donga.com/poll/score.html"]').remove();
+$(doc).ready(function() {
+	// Remove AD-Wrapper
+	var adContainer = doc.querySelectorAll('.ad_left_w,.ad_left_w2,.ad_right_w');
+	for (var i = 0, adContainerLen = adContainer.length; i < adContainerLen; i++){
+		adContainer[i].innerHTML = '';
+	}
+
+	// Remove Ad-Frame
+	var adFrame = doc.querySelectorAll('iframe[src*="donga.com"]');
+	for (var i = 0, adFrameLen = adFrame.length; i < adFrameLen; i++){
+		var t = adFrame[i];
+		t.parentNode.removeChild(t);
+	}
 
 	//user toolbox remove
-	$(document).on('click',function(){
+	$(doc).on('click',function(){
 		$('div[id^="nik_"]').css('display','none');
 	});
 });
