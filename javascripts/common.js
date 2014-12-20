ad40faa97d27723f5e6907842e0ea4ff11a47428
Cho WonEob <1up@1upz.com>
2014-12-21 03:37:10 +0900

    self invoking function 으로 변경

--- javascripts/common.js
+++ javascripts/common.js
@@ -16,25 +16,27 @@ chrome.extension.sendMessage({action:'width'}, function(response) {
 	}
 });
 
-//user toolbox remove
-doc.onclick = function(){
-	var userMenu = doc.querySelectorAll('div[id^="nik_"]');
-	for (var i = 0, userMenuLen = userMenu.length; i < userMenuLen; i++){
-		userMenu[i].style.display = 'none';
-	}
-};
+(function($, doc) {
+	//user toolbox remove
+	doc.onclick = function(){
+		var userMenu = doc.querySelectorAll('div[id^="nik_"]');
+		for (var i = 0, userMenuLen = userMenu.length; i < userMenuLen; i++){
+			userMenu[i].style.display = 'none';
+		}
+	};
 
-$(doc).ready(function() {
-	// Remove AD-Wrapper
-	var adContainer = doc.querySelectorAll('.ad_left_w,.ad_left_w2,.ad_right_w');
-	for (var i = 0, adContainerLen = adContainer.length; i < adContainerLen; i++){
-		adContainer[i].innerHTML = '';
-	}
+	$(doc).ready(function() {
+		// Remove AD-Wrapper
+		var adContainer = doc.querySelectorAll('.ad_left_w,.ad_left_w2,.ad_right_w');
+		for (var i = 0, adContainerLen = adContainer.length; i < adContainerLen; i++){
+			adContainer[i].innerHTML = '';
+		}
 
-	// Remove Ad-Frame
-	var adFrame = doc.querySelectorAll('iframe[src*="donga.com"], #ADhead1');
-	for (var i = 0, adFrameLen = adFrame.length; i < adFrameLen; i++){
-		var t = adFrame[i];
-		t.parentNode.removeChild(t);
-	}
-});
+		// Remove Ad-Frame
+		var adFrame = doc.querySelectorAll('iframe[src*="donga.com"], #ADhead1');
+		for (var i = 0, adFrameLen = adFrame.length; i < adFrameLen; i++){
+			var t = adFrame[i];
+			t.parentNode.removeChild(t);
+		}
+	});
+})(jQuery, document);
