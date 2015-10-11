d998bfb628b57916bd3355ce0504d7a4fd065274
1UP <1up@1upz.com>
2015-10-11 17:19:46 +0900

    self-Invoking 함수로 변경

--- javascripts/popup.js
+++ javascripts/popup.js
@@ -1,117 +1,120 @@
-var doc = document;
-var form = doc.forms.popupForm;
-var formElements = form.elements;
-var formElementLength = formElements.length;
-var ls = localStorage;
+;(function(win, doc) {
+	var form = doc.forms.popupForm;
+	var formElements = form.elements;
+	var formElementLength = formElements.length;
+	var ls = localStorage;
 
-function restore() {
-	for (var i = 0; i < formElementLength; i++) {
-		var thisElem = formElements[i];
+	var restore = function() {
+		for (var i = 0; i < formElementLength; i++) {
+			var thisElem = formElements[i];
 
-		if (ls[thisElem.name] === 'true') {
-			thisElem.checked = true;
-			thisElem.parentNode.classList.add('checked');
+			if (ls[thisElem.name] === 'true') {
+				thisElem.checked = true;
+				thisElem.parentNode.classList.add('checked');
+			}
 		}
-	}
-}
+	};
 
-var timeout;
-function showMessage(message) {
-	var messageBox = doc.getElementById('message'),
+	var timeout;
+	var showMessage = function(message) {
+		var messageBox = doc.getElementById('message');
 
-	show = function() {
-		messageBox.innerText = message;
-		messageBox.style.display = 'block';
-	},
+		var show = function() {
+			messageBox.innerText = message;
+			messageBox.style.display = 'block';
+		};
 
-	hide = function() {
-		messageBox.removeAttribute('style');
-	},
+		var hide = function() {
+			messageBox.removeAttribute('style');
+		};
 
-	init = (function() {
 		show();
-		clearTimeout(timeout);
-		timeout = setTimeout(hide, 1000);
-	})();
-}
 
-function postMessagenger(elem, inputElem, actionName) {
-	var input = formElements[inputElem];
-
-	elem.addEventListener('click', function() {
-		if (!input.value.trim()) return;
-
-		window.postMessage({
-			action: actionName,
-			content: input.value,
-			inputName: input.id
-		}, '*');
-	}, false);
-}
-
-function pressEnter(selector) {
-	var txtInputs = doc.querySelectorAll(selector);
-	var press = function(e) {
-		if (e.keyCode == 13) {
-			this.nextElementSibling.click();
+		if (timeout) {
+			clearTimeout(timeout);
 		}
+
+		timeout = setTimeout(hide, 1000);
 	};
 
-	for (var i = 0, len = txtInputs.length; i < len; i++) {
-		txtInputs[i].addEventListener('keyup', press, false);
+	function postMessagenger(elem, inputElem, actionName) {
+		var input = formElements[inputElem];
+
+		elem.addEventListener('click', function() {
+			if (!input.value.trim()) return;
+
+			win.postMessage({
+				action: actionName,
+				content: input.value,
+				inputName: input.id
+			}, '*');
+		}, false);
 	}
-}
 
-function checkboxChange() {
-	var checkboxes = doc.querySelectorAll('[type="checkbox"]');
+	function pressEnter(selector) {
+		var txtInputs = doc.querySelectorAll(selector);
+		var press = function(e) {
+			if (e.keyCode == 13) {
+				this.nextElementSibling.click();
+			}
+		};
 
-	var changer = function() {
-		ls[this.name] = this.checked;
-		this.parentNode.classList.toggle('checked');
-
-		showMessage('저장되었습니다.');
-	};
-
-	for (var i = 0, len = checkboxes.length; i < len; i++) {
-		checkboxes[i].addEventListener('change', changer, false);
+		for (var i = 0, len = txtInputs.length; i < len; i++) {
+			txtInputs[i].addEventListener('keyup', press, false);
+		}
 	}
-}
 
-doc.addEventListener('DOMContentLoaded', function() {
-	restore();
+	function checkboxChange() {
+		var checkboxes = doc.querySelectorAll('[type="checkbox"]');
 
-	postMessagenger(formElements.blockBtn, 'blockInput', 'titleBlockDelivery');
-	postMessagenger(formElements.blockUserBtn, 'blockUserInput', 'userBlockDelivery');
+		var changer = function() {
+			ls[this.name] = this.checked;
+			this.parentNode.classList.toggle('checked');
 
-	pressEnter('.keywordInput');
-	checkboxChange();
+			showMessage('저장되었습니다.');
+		};
 
-	chrome.management.get(chrome.i18n.getMessage('@@extension_id'), function(result) {
-		doc.getElementById('version').innerText = 'ver. ' + result.version;
-	});
-}, false);
+		for (var i = 0, len = checkboxes.length; i < len; i++) {
+			checkboxes[i].addEventListener('change', changer, false);
+		}
+	}
 
-window.addEventListener('message', function(e) {
-	if (window != e.source) return;
+	doc.addEventListener('DOMContentLoaded', function() {
+		restore();
 
-	switch(e.data.action) {
-		case 'titleBlockDelivery':
-		case 'userBlockDelivery':
-			chrome.extension.sendMessage(
-				{
-					action: e.data.action,
-					data: e.data
-				},
-				function(response) {
-					formElements[e.data.inputName].value = '';
+		postMessagenger(formElements.blockBtn, 'blockInput', 'titleBlockDelivery');
+		postMessagenger(formElements.blockUserBtn, 'blockUserInput', 'userBlockDelivery');
 
-					if(response.result) {
-						showMessage('저장되었습니다.');
-					} else {
-						showMessage(response.message);
+		pressEnter('.keywordInput');
+		checkboxChange();
+
+		chrome.management.get(chrome.i18n.getMessage('@@extension_id'), function(result) {
+			doc.getElementById('version').innerText = 'ver. ' + result.version;
+		});
+	}, false);
+
+	win.addEventListener('message', function(e) {
+		if (win != e.source) return;
+
+		switch(e.data.action) {
+			case 'titleBlockDelivery':
+			case 'userBlockDelivery':
+				chrome.extension.sendMessage(
+					{
+						action: e.data.action,
+						data: e.data
+					},
+					function(response) {
+						formElements[e.data.inputName].value = '';
+
+						if(response.result) {
+							showMessage('저장되었습니다.');
+						} else {
+							showMessage(response.message);
+						}
 					}
-				}
-			);
-		break;
-	}
-}, false);
+				);
+			break;
+		}
+	}, false);
+})(window, document);
